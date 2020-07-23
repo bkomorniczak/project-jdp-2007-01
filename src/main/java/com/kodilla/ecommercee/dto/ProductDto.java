e7cc7a017984022783af5844a636871fa1cf1ad1
nasz_login_do_github <nasz_email_do_github>
2020-07-23 14:24:36 +0200

    Implementacja Lomboka

--- src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/ProductDto.java
@@ -1,39 +1,16 @@
 package com.kodilla.ecommercee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
     private int id;
     private String name;
     private  String description;
     private int price;
     private String groupId;
-    public ProductDto() {
-    }
-
-    public ProductDto(int id, String name, String description, int price, String groupId) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.groupId = groupId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
 }
