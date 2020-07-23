ac0cf5a37db473f8b131d60630606a2e7268623e
Fordon <fordon-86@o2.pl>
2020-07-23 23:01:59 +0200

    UserController 1.0

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,24 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/v1/user")
 public class UserController {
+
+    @PostMapping
+    public void createUser (@RequestBody UserDto userDto) {    }
+
+    @PostMapping
+    public void blockUserId (@RequestParam int userId){}
+
+    @PostMapping
+    public int generateToken (@RequestBody UserDto userDto) {
+        Random random = new Random();
+        return random.nextInt(10);
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+}
