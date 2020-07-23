352c31db472fc1ca47923f2d654e37e5804e5d04
hbartek230 <bhreniak@gmail.com>
2020-07-23 19:30:59 +0200

    Uzupełniony GroupController oraz stworzona klasa GroupDto

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dto.GroupDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping
+    public GroupDto getGroup(@RequestParam Long groupId) {
+        return new GroupDto();
+    }
+
+    @PostMapping
+    public void createGroup(@RequestBody GroupDto groupDto) {
+
+    }
+
+    @PutMapping
+    public GroupDto updateGroup(@RequestBody Long groupId) {
+        return new GroupDto();
+    }
+
+    @DeleteMapping
+    public void deleteGroup(@RequestParam Long groupId) {
+
+    }
 }
