98856095a9967daf772e609af0c9359c48b4e7b0
nasz_login_do_github <nasz_email_do_github>
2020-07-24 20:42:50 +0200

    OrderController

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/order")
 public class OrderController {
+
+    @PostMapping
+    public void createOrder(@RequestBody OrderDto orderDto) {
+
+    }
+
+    @PostMapping
+    public OrderDto updateOrder(@RequestBody Long orderId) {
+        return new OrderDto();
+
+    }
+
+    @GetMapping
+    public OrderDto getOrder(@RequestParam Long orderId) {
+        return new OrderDto();
+
+    }
+
+    @DeleteMapping
+    public void deleteOrder(@RequestParam Long orderId) {
+
+    }
+
+    @GetMapping
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+
+    }
 }
