2319893f064d9e7ac3f0b400840265357bf794f7
bkomorniczak <b.komorniczak@gmail.com>
2020-07-24 16:11:45 +0200

    Group Entity 1.0

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,40 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "GROUP")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private List<Product> productList;
+
+    public Group(String name){
+        this.name = name;
+        this.productList = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
