9dd14b1c3b56c9591ffcf4e8681971715c459ced
nasz_login_do_github <nasz_email_do_github>
2020-07-25 22:34:54 +0200

    OrderController1.1

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -16,24 +16,24 @@ public class OrderController {
 
     }
 
-    @GetMapping
+    @GetMapping(value = "getOrder")
     public OrderDto getOrder(@RequestParam Long orderId) {
         return new OrderDto();
 
     }
 
-    @DeleteMapping
+    @DeleteMapping(value = "deleteOrder")
     public void deleteOrder(@RequestParam Long orderId) {
 
     }
 
-    @PostMapping
+    @PostMapping(value = "createOrder")
     public OrderDto createOrder(@RequestBody OrderDto orderDto) {
         return new OrderDto();
 
     }
 
-    @PutMapping
+    @PutMapping(value = "updateOrder")
     public void updateOrder(@RequestBody Long groupId) {
 
     }
