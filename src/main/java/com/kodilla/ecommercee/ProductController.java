e0ee3a6abfbb4e6779a233fa4ef430096d56e78a
nasz_login_do_github <nasz_email_do_github>
2020-07-27 20:51:33 +0200

    ProductEncja1.2

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,34 +1,4 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.dto.ProductDto;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("/v1/product")
 public class ProductController {
-
-    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
-    public List<ProductDto> getProducts(){
-        return new ArrayList<>();
-    }
-
-    @GetMapping(value = "getProduct")
-    public  ProductDto getProduct(@RequestParam Long productId) {
-        return new  ProductDto();
-    }
-
-
-    @DeleteMapping(value = "deleteProduct")
-    public void delete(@RequestParam Long productId) {}
-
-    @PutMapping(value = "updateProduct")
-    public void updateProduct(@RequestParam ProductDto productDto) {
-
-    }
-
-    @PostMapping(value = "createProduct")
-    public void createProduct(@RequestBody ProductDto productDto) {}
 }
