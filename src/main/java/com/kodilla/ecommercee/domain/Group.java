06619a3d03a841dcaa70ce21437ee080c7ad565f
MichalJott <MichalJott@github.com>
2020-08-07 13:30:14 +0200

    JDP200701-18_Encja_Group_testy_rev2

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +18,6 @@ public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-
     private String name;
 
     @OneToMany(
@@ -37,7 +33,6 @@ public class Group {
         this.productList = new ArrayList<>();
 
     }
-
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +22,4 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
     @Override
     void deleteById(Long id);
-
-
 }
