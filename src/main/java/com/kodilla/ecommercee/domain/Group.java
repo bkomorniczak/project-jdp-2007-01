db64445bd40a5cf79f46e74eedd8c4e9006b00e1
nasz_login_do_github <nasz_email_do_github>
2020-08-08 21:30:38 +0200

    merge ot master

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,40 +1,38 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Setter
 @Entity(name = "PRODUCT_GROUP")
 public class Group {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @NotNull
-    @Column(name = "NAME")
     private String name;
 
-    /*@OneToMany(
+    @OneToMany(
             targetEntity = Product.class,
             mappedBy = "group",
             fetch = FetchType.LAZY,
             cascade = CascadeType.ALL
     )
-    private List<Product> productList;*/
+    private List<Product> productList;
 
-    public Group(String name){
+    public Group(String name) {
         this.name = name;
-       // this.productList = new ArrayList<>();
-    }
+        this.productList = new ArrayList<>();
 
+    }
     public void setName(String name) {
         this.name = name;
     }
