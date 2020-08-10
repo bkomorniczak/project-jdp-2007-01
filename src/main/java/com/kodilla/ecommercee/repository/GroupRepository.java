cbd2cb79c67434f832ae20aa2d3836a9712ab57e
MichalJott <MichalJott@github.com>
2020-08-10 20:39:12 +0200

    JDP200701-22_GroupController_Rzeczywista_implementacja_metod

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -1,6 +1,7 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Group;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
