56217de79da00a88ba346b85abdadf6856ed0f4c
Fordon <fordon-86@o2.pl>
2020-08-17 22:44:38 +0200

    UserController rzeczywista implementacja 1.8

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,11 +1,10 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.ProductDto;
 import com.kodilla.ecommercee.exception.CartNotFoundException;
 import com.kodilla.ecommercee.exception.ProductNotFoundException;
 import com.kodilla.ecommercee.dto.CartDto;
-import com.kodilla.ecommercee.dto.CartItemDto;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class CartController {
     public void createCart(@RequestBody CartDto cartDto){}
 
     @GetMapping(value = "getProducts")
-    public List<CartItemDto> getProducts(@RequestParam Long cartId) throws CartNotFoundException, ProductNotFoundException {
+    public List<ProductDto> getProducts(@RequestParam Long cartId) throws CartNotFoundException, ProductNotFoundException {
         return new ArrayList<>();
     }
 
