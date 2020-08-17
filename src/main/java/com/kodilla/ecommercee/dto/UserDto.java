e340164f8f6b90dedf14c154327c09680bd08077
Fordon <fordon-86@o2.pl>
2020-08-17 19:15:28 +0200

    UserController rzeczywista implementacja 1.6 (dodano konstruktora do klasy UserDto)

--- src/main/java/com/kodilla/ecommercee/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/UserDto.java
@@ -3,8 +3,10 @@ package com.kodilla.ecommercee.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
@@ -15,4 +17,11 @@ public class UserDto {
     private String status;
     private Long userKey;
 
+    public UserDto(String userName, String userEmail, String userPassword, String status, Long userKey) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.status = status;
+        this.userKey = userKey;
+    }
 }
